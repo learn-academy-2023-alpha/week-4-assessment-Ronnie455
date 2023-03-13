9b664e1c0ff90d5cfb85c2071122b21dcdc703d3
Ronnie455 <ronniemaynard4@gmail.com>
2023-03-13 08:59:38 -0700

    incomplete test. unforseen circumstances this weekend. Will have the rest completed today.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,19 +12,47 @@
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
-// a) Create a test with an expect statement using the variable provided.
-// HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
+// I got confused with the invocation process by saying firsindex = color1.
+// I've been stuck on removing the first index and shuffling. I've created a variable within the function array2 that uses .shift() which removes the first element. I've also created a variable called shuffled array which randomizes the values within the array. In my return statement I am calling on the firstIndex and shuffled array which should return the first index of the array removed and shuffle the remaining values. Instead I'm just getting the array randomized. 
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+const colors1 = ["purple", "blue", "green", "yellow", "pink"];
+
+
+// const array2 = (array) => {
+//   const firstIndex = array.shift();
+//   const shuffledArray = array.sort((a , b) => Math.random() - 0.5);
+//   return [firstIndex, ...shuffledArray];
+// };
+
+// console.log(array2(colors1));
+  
+
+// // a) Create a test with an expect statement using the variable provided.
+// // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
+
+// describe('arrayContaining', () => {
+  
+//   const expected = ["purple", "blue", "green", "yellow", "pink"];
+//   it('matches even if received contains additional elements', () => {
+//     expect(["purple", "blue", "green", "yellow", "pink"]).toEqual(expect.arrayContaining(expected));
+//   });
+//   it('does not match if received does not contain expected elements', () => {
+//     expect(["blue", "green", "yellow", "pink"]).not.toEqual(expect.arrayContaining(expected));
+//   });
+// });
+
+
+
+
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = [
-  "chartreuse",
-  "indigo",
-  "periwinkle",
-  "ochre",
-  "aquamarine",
-  "saffron"
-]
+// const colors2 = [
+//   "chartreuse",
+//   "indigo",
+//   "periwinkle",
+//   "ochre",
+//   "aquamarine",
+//   "saffron"
+// ]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
 // b) Create the function that makes the test pass.
@@ -37,6 +65,16 @@ const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
+describe("expectedVotes", () => {
+  it("should take in an object that contains up votes and down votes and returns the end tally", () => {
+expect(expectedVotes(votes1)).toEqual(11);
+expect[expectedVotes(votes2)].toEqual(-31)
+  })
+})
+const expectedVotes = (votes) => {
+  return votes.upVotes = votes.downvotes
+}
+console.log(expectedVotes(votes1));
 
 // b) Create the function that makes the test pass.
 
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -2,33 +2,75 @@
 # MINASWAN ✌️
 
 # --------------------1) Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.
+#Pseudocode:
+
+# input - create a method with the variable even_or_odd and pass an argument into the parantheses called num
+# output - check to see if the number is even or odd
+# first create the method using the variable and argument. Create if statement using a modulo to check if the num evenly goes into 0. If it does the num is even. If not, the num is odd. Create return statement for bot odd and even using string interpolation.
+# Print for each num provided using the variable.
 
 num1 = 7
-# Expected output: '7 is odd'
 num2 = 42
-# Expected output: '42 is even'
 num3 = 221
+def even_or_odd(num)
+    if num % 2 == 0
+        return "#{num} even"
+    else
+        return "#{num} is odd"
+    end
+end
+p even_or_odd(num1)
+p even_or_odd(num2)
+p even_or_odd(num3)
+# Expected output: '7 is odd'
+#output: "7 is odd"
+# Expected output: '42 is even'
+#output: "42 even"
 # Expected output: '221 is odd'
-
+#"221 is odd"
 
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
 # HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
 
 beatles_album1 = 'Rubber Soul'
+# output: "Rbbr Sl"
 # Expected output: 'Rbbr Sl'
 beatles_album2 = 'Sgt Pepper'
+# output: 'Sgt Pppr'
 # Expected output: 'Sgt Pppr'
 beatles_album3 = 'Abbey Road'
+# output: 'bby Rd'
 # Expected output: 'bby Rd'
-
-
-
+def vowel_remover(str)
+    str.delete'aeiouAEIOU'
+end
+p vowel_remover(beatles_album1)
+p vowel_remover(beatles_album2)
+p vowel_remover(beatles_album3)
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
 
 palindrome_tester1 = 'Racecar'
+
 # Expected output: 'Racecar is a palindrome'
+# output: "Racecar is not a palindrome"
+# BUT by changing the lowercase r to a capitalized R it will come back as a palindrome.
 palindrome_tester2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
+# output: "LEARN is not a palindrome."
 palindrome_tester3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+# Output: "Rotator is not a palindrome"
+# Same thing as racecar. By changing the lower case r to a uppercase R it will read RotatoR is a palindrome.
+
+def palindrome?(string)
+    if string == string.reverse
+        return "#{string} is a palindrome"
+    else
+        return "#{string} is not a palindrome."
+    end
+
+end
+p palindrome?(palindrome_tester1)
+p palindrome?(palindrome_tester2)
+p palindrome?(palindrome_tester3)
